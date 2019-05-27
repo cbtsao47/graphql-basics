5c849961da0ef0d14fc8a698bdf0f5260036f6f4
wisdomtooth <cbtsao47@gmail.com>
2019-05-27 14:43:30 -0500

    complete auth

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
-
+// resolvers
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 const resolvers = {
-  Query: {
-    info: () => `This is the API of hackernews Clone`,
-    feed: (root, args, context, info) => {
-      console.log(context);
-      return context.prisma.links();
-    }
-  },
-  Mutation: {
-    post: (root, { url, description }, context, info) => {
-      return context.prisma.createLink({
-        url: url,
-        description: description
-      });
-    },
-    updateLink: (root, { id, url, description }, context, info) => {
-      return context.prisma.updateLink({
-        where: { id: id },
-        data: {
-          url: url,
-          description: description
-        }
-      });
-    },
-    deleteLink: (root, { id }, context, info) => {
-      return context.prisma.deleteLink({ id });
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
+  // Query: {
+  //   info: () => `This is the API of hackernews Clone`,
+  //   feed: (root, args, context, info) => {
+  //     console.log(context);
+  //     return context.prisma.links();
+  //   }
+  // },
+  // Mutation: {
+  //   post: (root, { url, description }, context, info) => {
+  //     return context.prisma.createLink({
+  //       url: url,
+  //       description: description
+  //     });
+  //   },
+  //   updateLink: (root, { id, url, description }, context, info) => {
+  //     return context.prisma.updateLink({
+  //       where: { id: id },
+  //       data: {
+  //         url: url,
+  //         description: description
+  //       }
+  //     });
+  //   },
+  //   deleteLink: (root, { id }, context, info) => {
+  //     return context.prisma.deleteLink({ id });
+  //   }
+  // }
 };
 async function main() {
   const newLink = await prisma.createLink({
